deed800394de19c457ecb3e5aeffb9d0a4d662d5
a lan <bluenowte97@gmail.com>
2018-11-05 21:10:31 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,8 @@ def using_push(array, string)
   array.push(string)
   #will return updated array
 end  
+
+def using_unshift(array, string)
+  array.unshift(string)
+  #will return updated array
+end  
