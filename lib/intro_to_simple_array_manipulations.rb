26bf181b984c431c4b1e10855b34f247d000fde7
a lan <bluenowte97@gmail.com>
2018-11-05 21:10:36 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,9 @@
 def using_push(array, string)
   array.push(string)
   #will return updated array
-end  
+end
 
 def using_unshift(array, string)
   array.unshift(string)
   #will return updated array
-end  
+end
