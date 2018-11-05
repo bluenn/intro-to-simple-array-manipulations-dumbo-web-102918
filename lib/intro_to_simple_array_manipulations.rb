db78a682482374556e855267e04319d6c3af02f5
a lan <bluenowte97@gmail.com>
2018-11-05 21:23:37 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,5 +16,11 @@ end
 def pop_with_args(array)
 array.pop(2)
 #the argument specifies how many elements, counting from the back of the array, to remove.
+#in this example we remove 2 elements
 #returns the deleted elements!!
 end
+
+def using_shift(array)
+  array.shift
+  #returns deleted first item!!
+end  
